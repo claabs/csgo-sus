6748227912680e545d3ed1e02bb474012ec57f08
Charlie Laabs <charlielaabs@gmail.com>
2022-01-26 21:41:05 -0600

    Switch to better-sqlite for cache

--- src/common/util.ts
+++ src/common/util.ts
@@ -84,9 +84,6 @@ export const getCache = (props?: GetCacheProps): Keyv => {
   const keyv = new Keyv(`sqlite://${getCacheDir()}/csgo-sus-cache.sqlite`, {
     namespace: props?.namespace,
     ttl: props?.ttl,
-    // Single quotes in the JSON object break the DB query. We have to base64 encode it to use Keyv properly.
-    serialize: (data) => Buffer.from(JSON.stringify(data), 'utf8').toString('base64'),
-    deserialize: (data) => JSON.parse(Buffer.from(data, 'base64').toString('utf8')),
   });
   keyv.on('error', (err) => L.error(err));
   return keyv;
